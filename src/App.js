5e1b3901b2bd195e5e409e5bb3af0ad279163068
Bouyahi Mohamed <bouyahi.mohamed.1@gmail.com>
2025-08-09 16:46:49 +0200

    run a Template html and css (Amazon Template )  in react using components

---
+++ src/App.js
@@ -0,0 +1,12 @@
+
+import Index from './components/index/Index.js';
+import './styles/shared/general.css'
+function App() {
+  return (
+    <div className="App">
+      <Index />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/index/Index.js
@@ -0,0 +1,11 @@
+import Header from './Header-Amazon.js';
+import ProductList from './ProductList.js';
+
+export default function Index() {
+  return (
+    <div>
+      <Header />
+      <ProductList />
+    </div>
+  );
+}
