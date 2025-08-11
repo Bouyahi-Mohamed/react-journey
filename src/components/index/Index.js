9efc5ace24afbde0a362f08fada65f23c582edcc
Bouyahi Mohamed <bouyahi.mohamed.1@gmail.com>
2025-08-11 19:33:51 +0200

    i added a new component product detaille using a my api for the data and i make the css too

--- src/components/index/Index.js
+++ src/components/index/Index.js
@@ -1,8 +1,12 @@
+import { useEffect,useState } from 'react';
 import Header from './Header-Amazon';
 import ProductList from './ProductList';
-import { useEffect,useState } from 'react';
+import ProductId from './ProductId';
+
 export default function Index() {
   const [products, setProducts] = useState([]);
+  const [product, setProduct] = useState(null);
+
 
   useEffect(() => {
     // fetch data
@@ -14,6 +18,7 @@ export default function Index() {
         return response.json();
       })
       .then(data => {
+        
         setProducts(data);
       })
       .catch(error => {
@@ -22,10 +27,33 @@ export default function Index() {
       });
   }, []);
 
+  useEffect(() => {
+    //fetch data
+    fetch('http://localhost:5000/products/3fdfe8d6-9a15-4979-b459-585b0d0545b9')
+      .then(response => {
+        if (!response.ok) {
+          throw new Error('Network response was not ok');
+        }
+        return response.json();
+      })
+      .then(data => {
+        console.log(data);
+        setProduct(data);
+      })
+      .catch(error => {
+        console.error('Error fetching products:', error);
+        setProduct(null);
+      });
+  }, []);
+
   return (
     <div>
       <Header />
-      <ProductList products={products} />
+      {/* <ProductList products={products} /> */}
+      {product && <ProductId product={product} />}
     </div>
   );
 }
+
+    
+
---
+++ src/components/index/ProductId.js
@@ -0,0 +1,38 @@
+import "../../styles/pages/produit.css";
+export default function ProductId({ product }) {
+  return (
+    <div className="container" key={product.id}>
+      <div className="product-image">
+        <img src={require(`../../${product.image}`)} alt={product.name} />
+      </div>
+      <div className="product-details">
+        <img
+          src={require(`../../images/ratings/rating-${
+            product.rating.stars * 10
+          }.png`)}
+          alt={`${product.rating.stars} stars`}
+        />
+        <h1>{product.name}</h1>
+
+        <h2>${(product.priceCents / 100).toFixed(2)}</h2>
+        <div className="product-description">
+          <h3>Description:</h3>
+          <p>{product.description}</p>
+        </div>
+        <div className="product-rating-count">
+          <div className="add-to-cart">
+            <select className="quantity-select">
+              <option value="1">1</option>
+              <option value="2">2</option>
+              <option value="3">3</option>
+              <option value="4">4</option>
+              <option value="5">5</option>
+            </select>
+            <button className="button-primary"> Add to Cart </button>
+          </div>
+          <div className="review-count">{product.rating.count} reviews</div>
+        </div>
+      </div>
+    </div>
+  );
+}
