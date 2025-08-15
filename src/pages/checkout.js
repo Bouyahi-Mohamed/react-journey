05ebcd59a9856a68d0c8022fe2c71863624ce0cc
Bouyahi Mohamed <bouyahi.mohamed.1@gmail.com>
2025-08-15 12:59:22 +0200

    refactor: pass carts data from api cart  to CheckoutList component for dynamic rendering and i test all the crud operation using postman

--- src/pages/checkout.js
+++ src/pages/checkout.js
@@ -1,11 +1,23 @@
 import Header2 from "../components/Header-2";
 import CheckoutList from "../components/Checkout-list";
+import {useState, useEffect } from "react";
 
 export default function Checkout() {
+  const [carts, setCarts] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:5000/cart/")
+    .then((response) => response.json())
+    .then((data) => {
+      // Handle the cart data
+      setCarts(data);
+    });
+  }, []);
+
   return (
     <div>
       <Header2 />
-      <CheckoutList />
+      <CheckoutList carts={carts} />
     </div>
   );
 }
